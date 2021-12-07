498347be844c24cc8321a5917137e063617d4008
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-12-07 09:52:04 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:quote_of_the_day/app.dart';
+
+void main() {
+  runApp(const App());
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,76 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> frase = [
+    'Pense sempre positivo.',
+    'Seja bom o tempo todo.',
+    'Não importa a situação, mantenha a calma!',
+    'Agradeça a Deus por mais um dia, pois Ele é bom.'
+  ];
+
+  String fraseDay = '';
+
+  void _randomFrase() {
+    Random random = Random();
+
+    setState(() {
+      fraseDay = frase[random.nextInt(frase.length)];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text('Frase do dia'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              fraseDay == ''
+                  ? Text('Aperte o botão para ver a frase',
+                      style: Theme.of(context).textTheme.bodyText1)
+                  : Text('A frase do dia para hoje é:',
+                      style: Theme.of(context).textTheme.bodyText1),
+              const SizedBox(height: 60),
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 16.0,
+                    vertical: 32,
+                  ),
+                  child: Text(
+                    fraseDay,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _randomFrase();
+        },
+        child: const Icon(Icons.rotate_right_outlined),
+      ),
+    );
+  }
+}
